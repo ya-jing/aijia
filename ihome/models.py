eae52151d2dff60bcb0d4ba9b4251b444f934ae5
ya-jing <liu_yajing10@163.com>
2020-03-27 00:03:12 +0800

    2020.3.26  七牛图片存储，用户中心，celery，房屋管理

--- ihome/models.py
+++ ihome/models.py
@@ -1,9 +1,10 @@
 # -*- coding:utf-8 -*-
 
 from datetime import datetime
-# from ihome import constants
 from . import db
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from ihome import constants
+
 
 class BaseModel(object):
     """模型基类，为每个模型补充创建时间与更新时间"""
@@ -27,23 +28,57 @@ class User(BaseModel, db.Model):
     houses = db.relationship("House", backref="user")  # 用户发布的房屋
     orders = db.relationship("Order", backref="user")  # 用户下的订单
 
+    # 加上property装饰器后，会把函数变为属性，属性名即为函数名
     @property
     def password(self):
-        """读取password属性时被调用"""
-        raise AttributeError("不可读")
+        """读取属性的函数行为"""
+        # print(user.password)  # 读取属性时被调用
+        # 函数的返回值会作为属性值
+        # return "xxxx"
+        raise AttributeError("这个属性只能设置，不能读取")
 
+    # 使用这个装饰器, 对应设置属性操作
     @password.setter
-    def password(self, passwd):
-        """设置密码时被调用,设置密码加密"""
-        self.password_hash = generate_password_hash(passwd)
+    def password(self, value):
+        """
+        设置属性  user.passord = "xxxxx"
+        :param value: 设置属性时的数据 value就是"xxxxx", 原始的明文密码
+        :return:
+        """
+        self.password_hash = generate_password_hash(value)
 
-    def check_password(self,passwd):
+    # def generate_password_hash(self, origin_password):
+    #     """对密码进行加密"""
+    #     self.password_hash = generate_password_hash(origin_password)
+
+    def check_password(self, passwd):
         """
         检验密码的正确性
-        :param passwd:用户登录时填写的原始密码
-        :return:如果正确，返回True，否则返回False
+        :param passwd:  用户登录时填写的原始密码
+        :return: 如果正确，返回True， 否则返回False
         """
-        return check_password_hash(self.password_hash,passwd)
+        return check_password_hash(self.password_hash, passwd)
+
+    def to_dict(self):
+        """将对象转换为字典数据"""
+        user_dict = {
+            "user_id": self.id,
+            "name": self.name,
+            "mobile": self.mobile,
+            "avatar": constants.QINIU_URL_DOMAIN + self.avatar_url if self.avatar_url else "",
+            "create_time": self.create_time.strftime("%Y-%m-%d %H:%M:%S")
+        }
+        return user_dict
+
+    def auth_to_dict(self):
+        """将实名信息转换为字典数据"""
+        auth_dict = {
+            "user_id": self.id,
+            "real_name": self.real_name,
+            "id_card": self.id_card
+        }
+        return auth_dict
+
 
 class Area(BaseModel, db.Model):
     """城区"""
@@ -53,6 +88,14 @@ class Area(BaseModel, db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 区域编号
     name = db.Column(db.String(32), nullable=False)  # 区域名字
     houses = db.relationship("House", backref="area")  # 区域的房屋
+
+    def to_dict(self):
+        """将对象转换为字典"""
+        d = {
+            "aid": self.id,
+            "aname": self.name
+        }
+        return d
 
 
 # 房屋设施表，建立房屋与设施的多对多关系
@@ -84,11 +127,71 @@ class House(BaseModel, db.Model):
     max_days = db.Column(db.Integer, default=0)  # 最多入住天数，0表示不限制
     order_count = db.Column(db.Integer, default=0)  # 预订完成的该房屋的订单数
     index_image_url = db.Column(db.String(256), default="")  # 房屋主图片的路径
-
     facilities = db.relationship("Facility", secondary=house_facility)  # 房屋的设施
-
     images = db.relationship("HouseImage")  # 房屋的图片
     orders = db.relationship("Order", backref="house")  # 房屋的订单
+
+    def to_basic_dict(self):
+        """将基本信息转换为字典数据"""
+        house_dict = {
+            "house_id": self.id,
+            "title": self.title,
+            "price": self.price,
+            "area_name": self.area.name,
+            "img_url": constants.QINIU_URL_DOMAIN + self.index_image_url if self.index_image_url else "",
+            "room_count": self.room_count,
+            "order_count": self.order_count,
+            "address": self.address,
+            "user_avatar": constants.QINIU_URL_DOMAIN + self.user.avatar_url if self.user.avatar_url else "",
+            "ctime": self.create_time.strftime("%Y-%m-%d")
+        }
+        return house_dict
+
+    def to_full_dict(self):
+        """将详细信息转换为字典数据"""
+        house_dict = {
+            "hid": self.id,
+            "user_id": self.user_id,
+            "user_name": self.user.name,
+            "user_avatar": constants.QINIU_URL_DOMAIN + self.user.avatar_url if self.user.avatar_url else "",
+            "title": self.title,
+            "price": self.price,
+            "address": self.address,
+            "room_count": self.room_count,
+            "acreage": self.acreage,
+            "unit": self.unit,
+            "capacity": self.capacity,
+            "beds": self.beds,
+            "deposit": self.deposit,
+            "min_days": self.min_days,
+            "max_days": self.max_days,
+        }
+
+        # 房屋图片
+        img_urls = []
+        for image in self.images:
+            img_urls.append(constants.QINIU_URL_DOMAIN + image.url)
+        house_dict["img_urls"] = img_urls
+
+        # 房屋设施
+        facilities = []
+        for facility in self.facilities:
+            facilities.append(facility.id)
+        house_dict["facilities"] = facilities
+
+        # 评论信息
+        comments = []
+        orders = Order.query.filter(Order.house_id == self.id, Order.status == "COMPLETE", Order.comment != None)\
+            .order_by(Order.update_time.desc()).limit(constants.HOUSE_DETAIL_COMMENT_DISPLAY_COUNTS)
+        for order in orders:
+            comment = {
+                "comment": order.comment,  # 评论的内容
+                "user_name": order.user.name if order.user.name != order.user.mobile else "匿名用户",  # 发表评论的用户
+                "ctime": order.update_time.strftime("%Y-%m-%d %H:%M:%S")  # 评价的时间
+            }
+            comments.append(comment)
+        house_dict["comments"] = comments
+        return house_dict
 
 
 class Facility(BaseModel, db.Model):
